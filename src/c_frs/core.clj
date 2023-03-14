ab5a33af381c3c2669eda651445864e3dbe013c0
david hanley <davidhanley@davids-MacBook-Air.local>
2023-03-14 15:36:03 -0700

    got race loading working, cool cool

--- src/c_frs/core.clj
+++ src/c_frs/core.clj
@@ -1,26 +1,66 @@
 (ns c-frs.core
   (:gen-class))
 
-(use 'clojure.string)
+(require '[clj-time.coerce :as c]
+         '[clojure.string :as str]
+         '[clojure.data.csv :as csv]
+         '[clojure.java.io :as io])
 
-(def fractions (map (fn [x] (/ 5 (+ 5 x))) (range)))
+(def fractions (map #(/ 5 (+ 5 %)) (range)))
 
 (defn get-scores-list-base [base]
-  (map (fn [x] (* x base)) fractions))
+  (map #(* % base) fractions))
 
 (def get-scores-list (memoize get-scores-list-base))
 
+(defn get-gender-from-string [gs]
+  (println gs)
+  ({\M :male, \F :female} (first (remove #(= % \*) gs))))
+
+(defn safe-parse-int [str]
+  (try
+    (Integer/parseInt str)
+    (catch Exception e nil)))
+
+(defn athlete-from-row [row]
+  (let [[_ name age-str gender-str] row
+        parsed-gender (get-gender-from-string gender-str)]
+    (if parsed-gender [{:athlete-name name :gender parsed-gender :age (safe-parse-int age-str)}] nil)))
+
+(def parse-date c/to-long)
+(defn get-race-from-strings [sheet-strings date-filter]
+  (let [[namestr datestr _ pointsstr & rest] sheet-strings
+        date (parse-date (first datestr))
+        points (safe-parse-int (first pointsstr))
+        header {:race-name (first namestr), :date date :race-points points}]
+    (if (and points date (date-filter date))
+      (doall (map #(conj header %) (mapcat athlete-from-row rest)))
+      [])))
+
+
+(defn clean-line [line] (map #(str/trim (str/upper-case %)) line))
 (defn read-file-into [filename pfunc]
   (with-open [rdr (clojure.java.io/reader filename)]
-    (pfunc (line-seq rdr))))
+    (doall (pfunc (map clean-line (csv/read-csv rdr))))))
 
 (defn scan-directories []
   (->>
     (clojure.java.io/file "TowerRunningRaceData")
     (file-seq)
-    (filter (fn [x] (ends-with? x ".csv")))))
+    (map str)
+    (filter (fn [x] (clojure.string/ends-with? x ".csv")))))
+
+(defn read-race [fn filter]
+  (read-file-into fn #(get-race-from-strings % filter)))
+
+(defn main-loop []
+  (->>
+    (scan-directories)
+    (pmap read-race)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (main-loop)
+  (shutdown-agents)
+  )
--- test/c_frs/core_test.clj
+++ test/c_frs/core_test.clj
@@ -10,11 +10,33 @@
   (testing "score list"
     (is (= (take 3 (get-scores-list 100)) [100 500/6 500/7]))))
 
-(deftest read-file-into-test
-  (testing "file reader"
-    (let [pf (fn [x] (doall (map println x)))]
-           (read-file-into "README.md" pf))))
-
 (deftest test-dir-scanner
   (testing "dir reader"
     (is (> (count (scan-directories)) 4))))
+
+
+
+(deftest test-gender
+  (testing "see if gender converter works"
+    (is (= (get-gender-from-string "M") :male))
+    (is (= (get-gender-from-string "F") :female))
+    (is (= (get-gender-from-string "MALE") :male))
+    (is (= (get-gender-from-string "FEMALE") :female))
+    (is (= (get-gender-from-string "") nil))
+    (is (= (get-gender-from-string "ZZ") nil))
+    ))
+
+(deftest test-athlete-from-row
+  (testing "test athlete from row"
+    (let [ath (athlete-from-row ["1","David Hanley","50","Male"])])))
+
+
+(deftest test-read-race
+  (testing "see if we read a race sanely"
+    (let [race (read-race "TowerRunningRaceData/2021-esbru.csv" #(if % true true))
+          first-race (first race)
+          ]
+      (doseq [ath race]
+        (println ath))
+
+      )))
